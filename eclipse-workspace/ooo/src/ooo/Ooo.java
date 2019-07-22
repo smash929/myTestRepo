ddcca17dde5876c88a1c50c421d4008ae503859c
smash470 <smrtnnd9804@gmail.com>
2019-07-22 15:53:24 +0200

    Pushing and Commiting ooo message!

---
+++ eclipse-workspace/ooo/src/ooo/Ooo.java
@@ -0,0 +1,12 @@
+package ooo;
+
+public class Ooo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("I love Kit Kat and i am awesome!!");
+		
+	}
+
+}
